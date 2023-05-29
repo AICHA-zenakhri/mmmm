75434db59e62091ddddb52d85c3e194bc4934fdc
ManelZy <124919778+ManelZy@users.noreply.github.com>
2023-05-30 00:17:09 +0100

    Update fooDZ-3.sql

--- fooDZ-3.sql
+++ fooDZ-3.sql
@@ -84,22 +84,84 @@ INSERT INTO `order_items` (`order_id`, `dish_id`, `item_quantity`) VALUES
 -- Table structure for table `products`
 --
 
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 30, 2023 at 01:15 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fooDz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
 CREATE TABLE `products` (
   `dish_id` int(11) NOT NULL,
-  `dish_name` varchar(255) NOT NULL,
-  `dish_desc` varchar(255) DEFAULT NULL,
-  `dish_price` decimal(10,2) NOT NULL,
-  `dish_img` varchar(255) DEFAULT NULL,
-  `dish_categ` varchar(255) NOT NULL,
+  `dish_name` varchar(50) NOT NULL,
+  `dish_desc` varchar(50) NOT NULL,
+  `dish_img` varchar(255) NOT NULL,
+  `dish_price` int(10) NOT NULL,
+  `dish_categ` varchar(30) DEFAULT NULL,
   `restaurant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `products`
+
+
+--
+-- Indexes for dumped tables
 --
 
-INSERT INTO `products` (`dish_id`, `dish_name`, `dish_desc`, `dish_price`, `dish_img`, `dish_categ`, `restaurant`) VALUES
-(5, 'sdsdn', 'dsd', '10110.00', 'uploads/6474b3a87a1477.43002354.jpg', 'dsdsse', 0);
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`dish_id`),
+  ADD KEY `fk3_restaurant` (`restaurant`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `dish_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk3_restaurant` FOREIGN KEY (`restaurant`) REFERENCES `restaurant` (`restaurant_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 -- --------------------------------------------------------
 
